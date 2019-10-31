9930c081b0d562ed3b7fe8a9d128491256cad8bb
Tyler-A <Tyler.akins1999@gmail.com>
2019-10-30 23:31:19 -0600

    Don't send messages to Discord log channel when in dev environment.

--- src/webhook.ts
+++ src/webhook.ts
@@ -3,14 +3,16 @@ import * as config from "./config.json";
 
 
 export const PUSH = (payload: any) => {
-    let options = {
-            uri: config.ERROR_WEBHOOK,
-            body: payload,
-            json: true
-        };
+    if (config.dev) { return; };
 
-        requests.post(options)
-        .catch((error: any) => {
-            console.error("OHNO, Shit Went DOWWWNNNNNN");
-        });
+    let options = {
+        uri: config.ERROR_WEBHOOK,
+        body: payload,
+        json: true
+    };
+
+    requests.post(options)
+    .catch((error: any) => {
+        console.error("OHNO, Shit Went DOWWWNNNNNN");
+    });
 }
