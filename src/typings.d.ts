c8e84e16c600149959a7af0d48d9aa34746850b5
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-02 13:33:33 -0600

    Add admin indicators in config

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,9 +1,18 @@
-interface twitch_options {
+interface base_service {
     OAUTH_TOKEN: string;
-    USERNAME: string;
-    CHANNELS: string[];
     SECRET: string;
     CLIENT_ID: string;
+    ADMIN: string[];
+}
+
+interface twitch_options extends base_service {
+    USERNAME: string;
+    CHANNELS: string[];
+}
+
+interface discord_options extends base_service {
+    PERMISSIONS_VALUE: number;
+    MOD_ROLES: string[];
 }
 
 
@@ -20,17 +29,18 @@ interface api_options {
 }
 
 
-interface discord_options {
-    OAUTH_TOKEN: string;
-    SECRET: string;
-    PERMISSIONS_VALUE: number;
-    CLIENT_ID: string;
-    MOD_ROLES: string[];
+interface config {
+    dev: boolean;
+    data_file: string;
+    chat: twitch_options;
+    discord: discord_options;
+    bot: bot_options;
+    api: api_options;
 }
-
 
 declare module "config.json" {
     const dev: boolean;
+    const data_file: string;
     const chat: twitch_options;
     const discord: discord_options;
     const bot: bot_options;
