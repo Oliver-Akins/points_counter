e5983b23c14a3242f6cb63119ebdccb4c829cf28
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-02 13:40:37 -0600

    Reorganize like... the entire command handler.

--- src/cmd_handler.ts
+++ src/cmd_handler.ts
@@ -1,19 +1,99 @@
-/* Commands */
-import { VERSION_COMMAND } from "./cmds/version";
-import { REMOVE_COMMAND } from "./cmds/remove";
-import { PING_COMMAND } from "./cmds/ping";
-import { HELP_COMMAND } from "./cmds/help";
-import { LEAD_COMMAND } from "./cmds/lead";
-import { LIST_COMMAND } from "./cmds/list";
-import { TOP3_COMMAND } from "./cmds/top";
-import { ADD_COMMAND } from "./cmds/add";
-import { ALL_COMMAND } from "./cmds/all";
+//
+// cmd_handler.ts
+// Protected under Canadian Copyright Laws
+//
+// Written by: Tyler Akins (2019/10/31)
+//
+
+
+/* Command Imports */
+import { ADD_SELECT, REMOVE_SELECT } from "./cmds/admin/select_commands";
+import { ADD_ALIAS, REMOVE_ALIAS } from "./cmds/admin/select_alias";
+import { ADMIN_HELP_COMMAND } from "./cmds/admin/admin_help";
+import { DATA_FILE_COMMAND } from "./cmds/admin/data_file";
+import { REMOVE_COMMAND } from "./cmds/mod/remove_points";
+import { VERSION_COMMAND } from "./cmds/user/version";
+import { ADD_COMMAND } from "./cmds/mod/add_points";
+import { PING_COMMAND } from "./cmds/user/ping";
+import { HELP_COMMAND } from "./cmds/user/help";
+import { LEAD_COMMAND } from "./cmds/user/lead";
+import { LIST_COMMAND } from "./cmds/user/list";
+import { TOP3_COMMAND } from "./cmds/user/top";
+import { ALL_COMMAND } from "./cmds/user/all";
 
 
 
-export function COMMAND_HANDLER (command: string, args: string[], is_mod=false): string|void {
+export function COMMAND_HANDLER (command: string, args: string[], is_mod=false, is_admin=false): string|void {
 
-    // SECTION: User Command
+    // SECTION: Admin Commands
+    if (command === "admin") {
+
+
+        // NOTE: Permission checking
+        if (!is_admin) { return "You don't have permission to run that command."; }
+
+        // NOTE: Subcommand check
+        else if (args.length < 1) { return "Not enough arguments, must specify a sub-command."; }
+
+
+        let subcommand: string = args[0];
+
+
+        // NOTE: data file subcommand
+        if (subcommand === "db") { return DATA_FILE_COMMAND(args); }
+
+        // NOTE: admin help
+        else if (subcommand === "help") { return ADMIN_HELP_COMMAND(); }
+
+
+        // SECTION: Alias subcommand
+        else if (subcommand === "alias") {
+            // !admin alias <target> <add|remove> <alias>
+            //          0      1         2           3
+            // NOTE: Ensure arguments
+            if (args.length < 4) { return "Not enough arguments, must specify an action."; }
+
+            // NOTE: Alias add
+            else if (args[2] === "add") { return ADD_ALIAS(args[1], args[3]); }
+
+            // NOTE: alias remove
+            else if (args[2] === "remove") { return REMOVE_ALIAS(args[1], args[3]); }
+        }
+        // !SECTION
+
+
+        // SECTION: Select subcommand
+        else if (["select", "option"].includes(subcommand)) {
+            // !admin option <add|remove> <alias>
+            //           0       1           2
+            // NOTE: Ensure arguments
+            if (args.length < 3) { return "Not enough arguments, must specify an action."; }
+
+            // NOTE: Alias add
+            else if (args[1] === "add") { return ADD_SELECT(args[2]); }
+
+            // NOTE: alias remove
+            else if (args[1] === "remove") { return REMOVE_SELECT(args[2]); }
+        }
+        //!SECTION
+    }
+    // !SECTION: Admin Commands
+
+
+
+    // SECTION: Moderator Commands
+    if (is_mod) {
+        // NOTE: add command
+        if (command === "add") { return ADD_COMMAND(args); }
+
+        // NOTE: remove command
+        else if (command === "remove") { return REMOVE_COMMAND(args); }
+    }
+    // !SECTION: Moderator Commands
+
+
+
+    // SECTION: User Commands
     // NOTE: list command
     if (command === "list") { return LIST_COMMAND(); }
 
@@ -34,19 +114,7 @@ export function COMMAND_HANDLER (command: string, args: string[], is_mod=false):
 
     // NOTE: all command
     else if (command === "all") { return ALL_COMMAND(); }
-    // !SECTION: User
-
-
-    // SECTION: Moderator Commands
-    // NOTE: Permission checking
-    else if (!is_mod) { return "You don't have permission to run that command!"; }
-
-    // NOTE: add command
-    else if (command === "add") { return ADD_COMMAND(args); }
-
-    // NOTE: remove command
-    else if (command === "remove") { return REMOVE_COMMAND(args); }
-    // !SECTION: Moderator
+    // !SECTION: User Commands
 
     return null;
 }
