308fa1f43cc37883120cceae2d695db705e9f7b7
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-12 01:43:08 -0600

    Start working on the command line arg.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,33 @@
 import { run_discord } from "./discord_handler";
 import { run_twitch } from "./twitch_handler";
-import { Command } from "./utils/Command";
+import { LOAD_CONFIG } from "./utils/Config";
 
 
-let meta: cmd_metadata = {
-    description: "Allows you to test to make sure the bot is online",
-    executable: (args: string[]) => { return ""; },
-    case_sensitive: false,
-    syntax: "{pre}ping",
-    name: "ping",
-    args: [],
-    level: 0
+let config = LOAD_CONFIG();
+let args = process.argv.slice(2);
+
+
+// Not enough arguments
+if (args.length < 1) {
+    console.error(
+        "Too few arguments, to get help with running the program, go to:",
+        config.WEBSITE + "/cli-args"
+    );
+    process.exit(1);
+}
+
+// Too many args
+else if (args.length > 1) {
+    console.error(
+        "Too many arguments given, to get help with running the program, go to:",
+        config.WEBSITE + "/cli-args"
+    );
+    process.exit(1);
 };
 
+let command = args[0];
 
-let cmd = new Command(meta);
 
-
-run_twitch()
+if (["run-all", "run-twitch"].includes(command)) {};
+if (["run-all", "run-discord"].includes(command)) {};
+if (["compile"].includes(command)) {};
