71ee1243de4bd44995835233ff1c34bfc7bb3e70
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-15 01:10:22 -0700

    Add the confirmation type and whether or not a command requires a confirmation to the command metadata.

--- src/types/command_metadata.d.ts
+++ src/types/command_metadata.d.ts
@@ -2,12 +2,16 @@
 // command_metadata.d.ts
 // Protected under Canadian Copyright Laws
 //
-// Written by: Tyler Akins (2019/11/06 - 2019/11/11)
+// Written by: Tyler Akins (2019/11/06 - 2019/11/12)
 //
+
+
+type CONFIRM_TYPE = "confirm"|"deny"|"no_match"|"expired"|"invalid";
 
 
 interface cmd_metadata {
     executable(context: msg_data, args: string[]): string|void;
+    requires_confirm: boolean;
     case_sensitive: boolean;
     description: string;
     opt_args: number;
