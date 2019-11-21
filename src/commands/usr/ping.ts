94e4169913adcfe11af1ecc99800ea1b2fab135a
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-20 23:52:56 -0600

    Remove confirmation from ping command and make it a perm.all level command.

---
+++ src/commands/usr/ping.ts
@@ -0,0 +1,28 @@
+//
+// ping.ts
+// Protected under Canadian Copyright Laws
+//
+// Written by: Tyler Akins (2019/11/06 - 2019/11/12)
+//
+
+import { REGISTER_COMMAND, confirms } from "../../cmd_handler";
+import { perm } from "../../constants";
+
+
+const PING_COMMAND = (context: msg_data, args: string[]): string|void => {
+    return "Pong!"
+};
+
+
+const ping_metadata: cmd_metadata = {
+    description: "Allows you to test to make sure the bot is online",
+    executable: PING_COMMAND,
+    requires_confirm: true,
+    case_sensitive: false,
+    syntax: "{pre}ping",
+    name: "ping",
+    opt_args: 0,
+    args: [],
+    level: perm.all
+};
+REGISTER_COMMAND(ping_metadata)
