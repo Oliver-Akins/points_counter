c61295aa778ce158062025116f6c0924738d27c4
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-21 13:01:34 -0600

    Start work on the list command.

--- src/commands/usr/list.ts
+++ src/commands/usr/list.ts
@@ -9,6 +9,23 @@
 import { LOAD } from "../../utils/db";
 
 
-const LIST_OPTIONS = (ctx: msg_data, args: string[]) => {
+const LIST_OPTIONS = (ctx: msg_data, args: string[]): string|void => {
     let channel = ctx.channel.replace(/\#/, "").replace(" ", "_");
+
+    let data = LOAD(channel);
+
+    let names: string[] = [];
+
+    for (var option of data) {
+        names.push(option.name);
+    };
+
+    let response = `Possible options: ${names.join(", ")}`;
+
+    switch (ctx.source) {
+        case "Discord":
+            break;
+        case "Twitch":
+            break;
+    }
 };
