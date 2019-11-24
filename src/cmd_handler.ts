0992861cdefbe606ae47ec49bda8ace5f4bb67de
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-23 23:04:57 -0600

    Add verison command

--- src/cmd_handler.ts
+++ src/cmd_handler.ts
@@ -169,5 +169,6 @@ export const HANDLE_MESSAGE = (context: msg_data): string | void => {
 
 /* Importing all the commands so they can register */
 import "./commands/usr/ping";
+import "./commands/usr/version";
 import "./commands/usr/options";
 import "./commands/admin/init_datafile";
---
+++ src/commands/usr/version.ts
@@ -0,0 +1,29 @@
+//
+// version.ts
+// Protected under Canadian Copyright Laws
+//
+// Written by: Tyler Akins (2019/11/23)
+//
+
+import { REGISTER_COMMAND } from "../../cmd_handler";
+import { LOAD_CONFIG } from "../../utils/Config";
+import { perm } from "../../constants";
+
+
+const VERSION_COMMAND = (context: msg_data, args: string[]): string|void => {
+    let config: config = LOAD_CONFIG();
+    return `Bot version: ${config.bot.VERSION}`;
+};
+
+
+const metadata: cmd_metadata = {
+    description: "Tells you the version of the bot you have.",
+    executable: VERSION_COMMAND,
+    requires_confirm: false,
+    case_sensitive: false,
+    name: "version",
+    opt_args: 0,
+    args: [],
+    level: perm.all
+};
+REGISTER_COMMAND(metadata)
