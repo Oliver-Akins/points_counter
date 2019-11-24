b162e2dc9d789e719b7288d287ae9914dd237b15
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-23 21:39:02 -0600

    Add error handling to Twitch commands.

--- src/services/twitch_handler.ts
+++ src/services/twitch_handler.ts
@@ -1,6 +1,7 @@
 import * as tmi from "tmi.js";
-import {LOAD_CONFIG} from "../utils/Config";
 import { HANDLE_MESSAGE } from "../cmd_handler";
+import { LOAD_CONFIG } from "../utils/Config";
+import { log_error } from "../utils/webhook";
 import { perm } from "../constants";
 
 /*
@@ -37,65 +38,108 @@ export const run_twitch = () => {
 
     // SECTION: Handle messages
     client.on("message", (channel: string, context: tmi.Userstate, message: string, self: boolean) => {
+        try {
+            // SECTION: Context checking
 
-        // SECTION: Context checking
-
-        // NOTE: Ensure not self
-        if (self) { return; }
-
-
-        // NOTE: Ensure message starts with prefix
-        else if (message.slice(0, config.bot.PREFIX.length) !== config.bot.PREFIX) { return; }
-        // !SECTION: Context checking
+            // NOTE: Ensure not self
+            if (self) { return; }
 
 
-        // SECTION: Context parsing
-        let is_admin = config.twitch.ADMIN.includes(context.username);
-        let is_mod = (
-            context.mod ||
-            context.badges ? context.badges.broadcaster === "1" : false ||
-            is_admin
-        )
-
-        var level = perm.all;
-        if (is_mod) { level = perm.mod; };
-        if (is_admin) { level = perm.admin; };
-        // !SECTION: Context parsing
+            // NOTE: Ensure message starts with prefix
+            else if (message.slice(0, config.bot.PREFIX.length) !== config.bot.PREFIX) { return; }
+            // !SECTION: Context checking
 
 
-        // NOTE: Get response
-        let response: string|void = HANDLE_MESSAGE({
-            message: message,
-            channel: channel,
-            level: level,
-            source: "Twitch",
-            user: context.username,
-            test: false
-        })
+            // SECTION: Context parsing
+            var is_admin = config.twitch.ADMIN.includes(context.username);
+            var is_mod = (
+                context.mod ||
+                context.badges ? context.badges.broadcaster === "1" : false ||
+                is_admin
+            )
+
+            var level = perm.all;
+            if (is_mod) { level = perm.mod; };
+            if (is_admin) { level = perm.admin; };
+            // !SECTION: Context parsing
 
 
-        // NOTE: Ensure response isn't null
-        if (response) {
-            client.say(
-                channel,
-                response as string
-            );
-        };
+            // NOTE: Get response
+            let response: string|void = HANDLE_MESSAGE({
+                message: message,
+                channel: channel,
+                level: level,
+                source: "Twitch",
+                user: context.username,
+                test: false
+            });
+
+
+            // NOTE: Ensure response isn't null
+            if (response) {
+                client.say(
+                    channel,
+                    response as string
+                );
+            };
+        } catch (error) {
+            log_error({
+                "embeds": [
+                    {
+                        "title": `${error.name}`,
+                        "color": 13238272,
+                        "description": `**Error Message:**\n\`\`\`\n${error.message}\n\`\`\``,
+                        "fields": [
+                            {
+                                "name": "**Message Context:**",
+                                "value": `\`\`\`\n${JSON.stringify(context, null, 2)}\n\`\`\``
+                            },
+                            {
+                                "name": "**Message Content:**",
+                                "value": `\`\`\`json\n${message}\`\`\``
+                            },
+                            {
+                                "name": "**Is Mod:**",
+                                "value": `\`${is_mod}\``,
+                                "inline": true
+                            },
+                            {
+                                "name": "**Is Admin:**",
+                                "value": `\`${is_admin}\``,
+                                "inline": true
+                            },
+                            {
+                                "name": "**Channel:**",
+                                "value": `\`${channel}\``,
+                                "inline": true
+                            },
+                            {
+                                "name": "Source",
+                                "value": "Twitch",
+                                "inline": true
+                            }
+                        ]
+                    }
+                ]
+            });
+        }
     });
     // !SECTION
 
 
     // A user is cheering us on!
-    /*client.on("cheer", (channel: string, context: tmi.Userstate, message: string) => {
-        // Do your stuff.
-        let bit_count: number = context.bits;
-    });
+    // client.on("cheer", (channel: string, context: tmi.Userstate, message: string) => {
+    //     // Do your stuff.
+    //     let bit_count: number = context.bits;
+    // });
 
     client.on("disconnected", (reason: string) => {
-        // Do your stuff.
-    });*/
+        console.log(`* Disconnected from Twitch`);
+    });
+
     client.on("connected", (addr: string, port: number) => {
         console.log(`* Connected to ${addr}:${port}`);
+        // client.say("#alkali_metal", "Connected to chat. I'm now active.");
     });
 
     client.connect().catch((err) => {throw err})
