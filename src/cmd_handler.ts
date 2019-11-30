e8e29d96b59aceea0de02e78c817c16703a365ad
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-30 14:45:07 -0600

    Update imports for top

--- src/cmd_handler.ts
+++ src/cmd_handler.ts
@@ -173,9 +173,9 @@ export const HANDLE_MESSAGE = (context: msg_data): string => {
 
 
 /* Importing all the commands so they can register */
+import "./commands/usr/top";
 import "./commands/usr/ping";
 import "./commands/usr/help";
-import "./commands/usr/lead";
 import "./commands/usr/version";
 import "./commands/mod/points_add";
 import "./commands/mod/options_add";
