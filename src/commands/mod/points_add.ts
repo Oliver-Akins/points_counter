ba8ba2711f31d4e9a0f9ad469b792a553d6a7f8b
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-30 14:43:43 -0600

    Add points add command.

---
+++ src/commands/mod/points_add.ts
@@ -0,0 +1,60 @@
+//
+// points_add.ts
+// Protected under Canadian Copyright Laws
+//
+// Written by: Tyler Akins (2019/11/29)
+//
+
+
+import { REGISTER_COMMAND } from "../../cmd_handler";
+import { LOAD, WRITE } from "../../utils/db";
+import { PERM } from "../../constants";
+
+// !points add 5 potato
+
+const POINTS_ADD_COMMAND = (ctx: msg_data, args: string[]): string => {
+    ctx.channel = ctx.channel.replace(/\#/, "").replace(" ", "_")
+
+    let data = LOAD(ctx.channel);
+
+    let amount = parseInt(args[0]);
+    let target = args[1];
+    let user = args[2] ? args[2] : "%anonymous%";
+
+
+    for (var option of data) {
+        if (option.aliases.includes(target.toLowerCase())) {
+
+            // Ensure user is defined before trying to add to undefined
+            if (option.points[user]) {
+                option.points[user] += amount;
+            } else {
+                option.points[user] = amount;
+            };
+            option.total += amount;
+
+            WRITE(ctx.channel, data);
+            return `${amount} points have been added to ${option.name} on behalf of ${user}.`;
+        };
+    };
+    return `Could not find an option of name \`${target}\``
+};
+
+
+
+const metadata: cmd_metadata = {
+    description: "Adds an option to the channel's point system.",
+    requires_confirm: false,
+    case_sensitive: false,
+    executable: POINTS_ADD_COMMAND,
+    opt_args: 1,
+    args: [
+        "<Amount: Integer>",
+        "<Option: String>",
+        "[User: String]"
+    ],
+    group: "points",
+    name: "add",
+    level: PERM.MOD
+};
+REGISTER_COMMAND(metadata);
