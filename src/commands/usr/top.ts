e81c678b09bf6eb6451f45eb15b9b752295a61a6
Tyler-A <Tyler.akins1999@gmail.com>
2019-12-03 00:01:03 -0600

    Update header.

--- src/commands/usr/top.ts
+++ src/commands/usr/top.ts
@@ -2,7 +2,7 @@
 // top.ts
 // Protected under Canadian Copyright Laws
 //
-// Written by: Tyler Akins (2019/11/29)
+// Written by: Tyler Akins (2019/11/29 - 2019/12/02)
 //
 
 import { REGISTER_COMMAND } from "../../cmd_handler";
