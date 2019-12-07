a444b8c36f34b8887cf30332215fd1b193e8a1f7
Tyler-A <Tyler.akins1999@gmail.com>
2019-12-07 13:54:40 -0600

    Remove TS lang files.

--- src/langs/ENG_CA.ts
+++
@@ -1,6 +0,0 @@
-//
-// ENG_CA.ts
-// Protected under Canadian Copyright Laws
-//
-// Written by: Tyler Akins ()
-//
--- src/langs/ENG_US.ts
+++
@@ -1,6 +0,0 @@
-//
-// ENG_US.ts
-// Protected under Canadian Copyright Laws
-//
-// Written by: Tyler Akins ()
-//
