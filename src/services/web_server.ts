82b6ae8045b80293239c279229144020c84fb56e
Tyler-A <Tyler.akins1999@gmail.com>
2019-12-08 22:11:31 -0600

    Initialize web server

---
+++ src/services/web_server.ts
@@ -0,0 +1,16 @@
+//
+// web_server.ts
+// Protected under Canadian Copyright Laws
+//
+// Written by: Tyler Akins (2019/12/08)
+//
+
+
+/*
+Templater For ExpressJS: https://github.com/Drulac/express-tl
+*/
+
+import * as express from "express";
+
+
+const server = express();
