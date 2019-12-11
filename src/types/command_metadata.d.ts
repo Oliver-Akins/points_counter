999bab4135a591d099eee76c50990ad275050100
Tyler-A <Tyler.akins1999@gmail.com>
2019-12-10 21:21:51 -0600

    Add metadata for the docs alert system

--- src/types/command_metadata.d.ts
+++ src/types/command_metadata.d.ts
@@ -2,17 +2,35 @@
 // command_metadata.d.ts
 // Protected under Canadian Copyright Laws
 //
-// Written by: Tyler Akins (2019/11/06 - 2019/11/12)
+// Written by: Tyler Akins (2019/11/06 - 2019/12/10)
 //
 
 
 type CONFIRM_TYPE = "confirm"|"deny"|"no_match"|"expired"|"invalid";
 
 
+
+interface positions {
+    head?: string;
+    head_2?: string;
+    table_head?: string;
+    table_foot?: string;
+    usage?: string;
+}
+
+interface alert_structure {
+    info?: positions;
+    warn?: positions;
+    error?: positions;
+}
+
+
+
 interface cmd_metadata {
     executable(context: msg_data, args: string[]): string;
     requires_confirm: boolean;
     case_sensitive: boolean;
+    alerts?: alert_structure;
     description: string;
     opt_args: number;
     group?: string;
