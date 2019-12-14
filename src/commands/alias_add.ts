7bfc56882613c1291fec057fd52d6b385111b4f7
Tyler-A <Tyler.akins1999@gmail.com>
2019-12-13 23:01:20 -0600

    Finish implementing alias add command.

--- src/commands/alias_add.ts
+++ src/commands/alias_add.ts
@@ -6,27 +6,32 @@
 //
 
 
-import { LOAD } from "../utils/db";
+import { LOAD, WRITE } from "../utils/db";
+import { RESOLVE_CHANNEL } from "../utils/metadata";
 
 
 const ALIAS_ADD = (ctx: msg_data, args: string[]): string => {
 
-    let data = LOAD(
-        ctx.channel.replace(/#/g, "").replace(/ /g, "_")
-    );
-
+    let channel = RESOLVE_CHANNEL(ctx);
+    let data = LOAD(channel);
     let target = args[0];
-    let target_data: option;
     let new_alias = args[1];
     let all_aliases: string[] = [];
+    let target_data: option;
 
-    // TODO: Check to make sure that the new_alias is completely unique
-    // TODO: Add the new alias to the data
 
     for (var option of data) {
         if (option.aliases.includes(target)) {
             target_data = option;
         };
-        all_aliases.push(...option.aliases)
-    }
+        all_aliases.push(...option.aliases);
+    };
+
+    if (all_aliases.includes(new_alias)) {
+        return `That alias is already in use.`
+    };
+
+    target_data.aliases.push(new_alias);
+    WRITE(channel, data);
+    return `Added \`${new_alias}\` to ${target_data.name}.`
 }
