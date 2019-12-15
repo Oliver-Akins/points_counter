e7101308bc4c9a3d4389ea8eb8ca50b9fe226551
Tyler-A <Tyler.akins1999@gmail.com>
2019-12-15 00:18:54 -0600

    Remove existing tests and update typing for the new test system.

--- src/types/message_metadata.d.ts
+++ src/types/message_metadata.d.ts
@@ -2,11 +2,15 @@
 // message_metadata.d.ts
 // Protected under Canadian Copyright Laws
 //
-// Written by: Tyler Akins (2019/11/07 - 2019/12/12)
+// Written by: Tyler Akins (2019/11/07 - 2019/12/14)
 //
 
+
+type platform = "Discord"|"Twitch"|"Tests"
+
+
 interface msg_data {
-    source: "Discord"|"Twitch";
+    source: platform;
     cooldown: boolean;
     channel: string;
     message: string;
--- src/types/test_object.d.ts
+++ src/types/test_object.d.ts
@@ -2,14 +2,25 @@
 // test_object.ts
 // Protected under Canadian Copyright Laws
 //
-// Written by: Tyler Akins (2019/11/17)
+// Written by: Tyler Akins (2019/11/17 - 2019/12/14)
 //
+
+
+interface test_msg_data {
+    source: platform;
+    channel: string;
+    message: string;
+    level: number;
+    user: string;
+}
 
 
 interface test {
     datafile_should_exist: "EXISTS"|"NOT_EXISTS"|"IGNORES";
-    confirmation_msg?: msg_data;
+    datafile_populated: boolean;
+    confirmation_msg?: test_msg_data;
     expected_return: string;
-    msg_meta: msg_data;
+    msg_meta: test_msg_data;
+    links: {[key: string]: string}
     id: string;
 }
