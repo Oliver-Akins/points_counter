6359aeba8e75a73d750f2fdb5b38e94266b69d92
Tyler-A <Tyler.akins1999@gmail.com>
2019-12-15 16:44:32 -0600

    Adjust typing of the test objects.

--- src/types/test_object.d.ts
+++ src/types/test_object.d.ts
@@ -2,23 +2,21 @@
 // test_object.ts
 // Protected under Canadian Copyright Laws
 //
-// Written by: Tyler Akins (2019/11/17 - 2019/12/14)
+// Written by: Tyler Akins (2019/11/17 - 2019/12/15)
 //
 
 
 interface test_msg_data {
     source: platform;
-    channel: string;
     message: string;
     level: number;
-    user: string;
 }
 
 
 interface test {
     datafile_should_exist: "EXISTS"|"NOT_EXISTS"|"IGNORES";
-    datafile_populated: boolean;
-    confirmation_msg?: test_msg_data;
+    datafile_populated?: boolean;
+    confirm_msg?: test_msg_data;
     expected_return: string;
     msg_meta: test_msg_data;
     links: {[key: string]: string}
